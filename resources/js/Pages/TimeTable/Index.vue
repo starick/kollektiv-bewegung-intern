a08ead7e0808ad24b1e992a6fbb4d9b78f974365
starick <jakob.starick@web.de>
2025-09-23 22:18:09 +0200

    basic pages

---
+++ resources/js/Pages/TimeTable/Index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import AppLayout from '@/Components/Layout/AppLayout.vue';
+</script>
+
+<template>
+  <AppLayout title="Dashboard">
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        Dashboard
+      </h2>
+    </template>
+
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+          Lorem Ipsum
+        </div>
+      </div>
+    </div>
+  </AppLayout>
+</template>
