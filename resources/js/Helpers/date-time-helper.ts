794c01210a393f00c0442d8fc5ee8c52578c502e
Jakob Starick <jakob.starick@web.de>
2025-10-13 16:47:07 +0200

    expand date-time-helper

--- resources/js/Helpers/date-time-helper.ts
+++ resources/js/Helpers/date-time-helper.ts
@@ -1,23 +1,31 @@
 import { addWeeks, endOfISOWeek, startOfISOWeek } from 'date-fns';
 
 export function formatDateTime(d: Date | string): string {
-  return new Date(d).toLocaleString('de-DE');
+  return new Date(d).toLocaleString('de-DE', { timeZone: 'UTC' });
 }
 
 export function formatDate(d: Date | string): string {
-  return new Date(d).toLocaleDateString('de-DE');
+  return new Date(d).toLocaleDateString('de-DE', { timeZone: 'UTC' });
 }
 
 export function formatTime(d: Date | string): string {
-  return new Date(d).toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit' });
+  return new Date(d).toLocaleTimeString('de-DE', {
+    hour: '2-digit',
+    minute: '2-digit',
+    timeZone: 'UTC'
+  });
 }
 
 export function formatDayAndMonth(d: Date | string): string {
-  return new Date(d).toLocaleDateString('de-DE', { day: '2-digit', month: '2-digit' });
+  return new Date(d).toLocaleDateString('de-DE', {
+    day: '2-digit',
+    month: '2-digit',
+    timeZone: 'UTC'
+  });
 }
 
 export function formatWeekday(d: Date | string): string {
-  return new Date(d).toLocaleDateString('de-DE', { weekday: 'short' });
+  return new Date(d).toLocaleDateString('de-DE', { weekday: 'short', timeZone: 'UTC' });
 }
 
 export function startOfWeek(year: number, week: number): Date {
@@ -27,4 +35,12 @@ export function startOfWeek(year: number, week: number): Date {
 
 export function endOfWeek(year: number, week: number): Date {
   return endOfISOWeek(startOfWeek(year, week));
+}
+
+export function formatInternationalDate(d: Date | string): string {
+  const date = new Date(d);
+
+  return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(
+    date.getDate()
+  ).padStart(2, '0')}`;
 }
